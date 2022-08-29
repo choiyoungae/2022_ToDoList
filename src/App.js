e9a7bf5b98cb1df0baedec3c337efb0ccba7a04c
choiyoungae <choiaeyoung1001@gmail.com>
2022-08-29 16:30:28 +0900

    ToDoList 만들기 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // 완료된 체크표시
+    // <i class="fa-solid fa-circle-check"></i>
+    // 쓰레기통
+    // <i class="fa-solid fa-trash-can"></i>
+    return <></>
 }
 
-export default App;
+export default App
