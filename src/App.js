1e761a1ce77d9f36e1877229dafd7ff65ce40141
choiyoungae <choiaeyoung1001@gmail.com>
2022-08-30 17:08:17 +0900

    시계 구현

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Clock from './Clock'
 const App = () => {
     // 완료된 체크표시
     // <i class="fa-solid fa-circle-check"></i>
-    // 쓰레기통
+    // 쓰레기통 
     // <i class="fa-solid fa-trash-can"></i>
     return <>
         <div id="clock">{Clock()}</div>
